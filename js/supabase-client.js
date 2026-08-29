761c583e786b02e4724c840f6b0e737139782e06
leomarlizcano928-dotcom <leomarlizcano928@gmail.com>
2026-08-29 02:12:09 -0500

    Delete js directory

--- js/supabase-client.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://aoyjxfvleeznadgemlti.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_TWI-Vgy9dE6JLcT1nQa_3g_wQUpp9wo";
-
-const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
